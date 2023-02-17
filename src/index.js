a5ef6ce45b3dff30da761c7ef81bc317695e1562
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-17 08:19:55 -0500

    Import express implement esmodules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world')
+import express from "express";
+
+const app = express();
+app.listen(3000);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+console.log('Server listening on port 3000');
