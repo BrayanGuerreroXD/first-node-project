94f18d6def6207579b71930a484c020de1f850ad
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-17 08:33:09 -0500

    Build the node server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname,'views'))
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
 app.listen(3000);
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-console.log('Server listening on port 3000');
+console.log("Server listening on port 3000");
