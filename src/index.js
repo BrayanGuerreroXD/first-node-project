e1e56a4531abaa6e4090228181505f8240969f48
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-17 13:49:46 -0500

    Add styles to bootstrap 5

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set("views", join(__dirname,'views'))
 app.set("view engine", "ejs");
+
 app.use(indexRoutes)
+app.use(express.static(join(__dirname,'public')))
 
 app.listen(3000);
 console.log("Server listening on port 3000");
